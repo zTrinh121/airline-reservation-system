1797f54fb07afd7aa03c7cae6b25a1b6b7315f08
phundanghoc <145929031+Phun1234@users.noreply.github.com>
2024-03-03 15:24:44 +0700

    Account

---
+++ src/java/model/Admin.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author HP
+ */
+public class Admin {
+    private String adminId;
+    private String password;
+    private String name;
+    private String email;
+
+    public Admin(String adminId, String password, String name, String email) {
+        this.adminId = adminId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Admin() {
+    }
+
+    public String getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(String adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}
