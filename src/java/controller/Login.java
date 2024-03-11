b5d660601bf86d4fc94edd4a0650cd5fc23cd500
Trinh Nguyen <137372274+zTrinh121@users.noreply.github.com>
2024-03-11 09:27:05 +0700

    User Interface check

--- src/java/controller/Login.java
+++ src/java/controller/Login.java
@@ -9,11 +9,11 @@ import dao.AccountDao;
 import dbcontext.ConnectDB;
 import java.io.IOException;
 import java.io.PrintWriter;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.sql.Connection;
 
 /**
