d285a528852ee21592c92e94fcd3a83132a1fcc6
Trinh Nguyen <137372274+zTrinh121@users.noreply.github.com>
2024-03-16 11:21:15 +0700

    Payment and mail

---
+++ src/java/dao/MailDAO.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import model.MailConfig;
+
+/**
+ *
+ * @author Trinh
+ */
+public class MailDAO {
+
+    public boolean send(String to, String subject, String messageContent) {
+        // Get properties object
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.host", MailConfig.HOST_NAME);
+        props.put("mail.smtp.socketFactory.port", MailConfig.SSL_PORT);
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.port", MailConfig.SSL_PORT);
+
+        // get Session
+        Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(MailConfig.APP_EMAIL, MailConfig.APP_PASSWORD);
+            }
+        });
+
+        // compose message
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject(subject);
+            message.setText(messageContent);
+
+            // send message
+            Transport.send(message);
+
+            return true;
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public static void main(String[] args) {
+        String to = "trinhntmde180146@fpt.edu.vn";
+        String subject = "Dang ky nhan thu thanh cong";
+        String message = "Cam on ban da dang ky nhan thu";
+        MailDAO md = new MailDAO();
+        md.send(to, subject, message);
+    }
+}
---
+++ src/java/model/MailConfig.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Trinh
+ */
+public class MailConfig {
+
+    public static final String HOST_NAME = "smtp.gmail.com";
+
+    public static final int SSL_PORT = 465; // Port for SSL
+
+    public static final int TSL_PORT = 587; // Port for TLS/STARTTLS
+
+    public static final String APP_EMAIL = "homeflight1304@gmail.com"; // your email
+
+    public static final String APP_PASSWORD = "qcaj oztm othq hldk"; // your password
+
+}
